b3b03b1d90eb2a9013cdc8329ac8eaedd9084b9a
su-5 <alert@DESKTOP-5UVGHBN>
2019-03-23 18:23:20 +0300

    add  kendo

--- TestTask/ClientApp/src/app/app.module.ts
+++ TestTask/ClientApp/src/app/app.module.ts
@@ -6,29 +6,65 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
+import { OrderComponent } from './order/order.component';
+import { UserComponent } from './user/user.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
+import { GridModule } from '@progress/kendo-angular-grid';
+import { WindowModule } from '@progress/kendo-angular-dialog';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { DialogModule, WindowRef } from '@progress/kendo-angular-dialog';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { PopupModule } from '@progress/kendo-angular-popup';
+import { ResizeSensorModule } from '@progress/kendo-angular-resize-sensor/dist/es2015';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+
+import { TreeViewModule } from '@progress/kendo-angular-treeview';
+import '@progress/kendo-angular-intl/locales/ru/all';
+import { MessageService } from '@progress/kendo-angular-l10n';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { TooltipModule } from '@progress/kendo-angular-tooltip';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { ChartsModule } from '@progress/kendo-angular-charts';
+
+import { UserService } from './services/user.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        OrderComponent,
+        UserComponent,
+        FetchDataComponent
+    ],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+        HttpClientModule,
+        FormsModule,
+        GridModule,
+        WindowModule,
+        //ButtonsModule,
+        //DialogModule,
+        //ChartsModule,
+        //DropDownsModule,
+        //TooltipModule,
+        //LayoutModule,
+        //DateInputsModule,
+        //InputsModule,
+        //BrowserAnimationsModule,
+        //TreeViewModule,
+        //PopupModule,
+        //ResizeSensorModule,
+        RouterModule.forRoot([
+            { path: '', component: OrderComponent, pathMatch: 'full' },
+            { path: 'user', component: UserComponent },
+            { path: 'fetch-data', component: FetchDataComponent },
+        ])
+    ],
+    providers: [UserService,
+     //   WindowRef, MessageService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ TestTask/ClientApp/src/app/order/order.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+})
+export class OrderComponent {
+}
